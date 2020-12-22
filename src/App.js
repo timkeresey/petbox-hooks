ad247ebc0fe688272410533740c10ab775e1c090
Tim Keresey <tim.keresey@gmail.com>
2020-12-22 12:58:13 -0700

    add state hooks to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-// import PetList from './PetList'
+import PetList from './PetList'
 import './App.css';
 
 function App() {
-  
+  const [pets, setPets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
   return (
     <div className='App'>
       <h1>PetBox</h1>
+      { error && error }
+      { isLoading ? <div>Loading...</div> : <PetList pets={pets} /> }
     </div>
   )
 }
