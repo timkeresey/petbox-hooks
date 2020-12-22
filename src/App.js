1297d899de9f0dc0a8a5e4bfb367c3511c4b6656
Tim Keresey <tim.keresey@gmail.com>
2020-12-22 13:41:06 -0700

    add useEffect hook to App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
+//declare api call function that uses state hooks to set local state of pets.
+  const getPets = async () => {
+    const url = 'http://localhost:3001/api/v1/pets';
+    setError('');
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const pets = await response.json();
+      setPets(pets);
+    } catch(error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  };
+
+// assign api call function to useEffect hook to give it componentDidMount characteristics.
+  // must return a function, cannot return a promise. This is why getPets is defined outside of useEffect and called within it
+  useEffect(() => {
+    getPets();
+  }, [])
+
   return (
     <div className='App'>
       <h1>PetBox</h1>
